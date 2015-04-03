4f664c9969dd7f4a39e68bc42b77b8844ece577d
Édipo Fernandes <edipofernandes7@gmail.com>
2015-04-03 13:43:00 -0300

    Adicionado os metódos para a classe pesquisador

--- src/main/java/pm/controller/PesquisadorControle.java
+++ src/main/java/pm/controller/PesquisadorControle.java
@@ -1,5 +1,70 @@
 package pm.controller;
 
-public class PesquisadorControle {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import pm.model.Pesquisador;
+
+public class PesquisadorControle {
+	private Collection<Pesquisador> listaPesquisadores;
+	private String caminhoArquivoEntrada = "";
+	
+	/*
+	 * Construtor da classe recebe um caminho para arquivo
+	 * como parametro e é criada uma lista de pesquisadores
+	 */
+	public PesquisadorControle(String caminhoArquivoEntrada){
+		this.caminhoArquivoEntrada = caminhoArquivoEntrada;
+		criarListaPesquisadores();
+	}
+	
+	private void criarListaPesquisadores(){
+		Collection<String> dadosArquivo = new ArrayList<String>();
+		try{
+			FileReader arq = new FileReader(caminhoArquivoEntrada);
+			BufferedReader lerArq = new BufferedReader(arq);
+			String linha = lerArq.readLine();
+			while(linha != null){
+				dadosArquivo.add(linha);
+				linha = lerArq.readLine();
+			}
+			
+			arq.close();
+			
+		}catch(IOException e){
+			System.out.println("Erro ao tentar abrir o arquivo\n");
+			e.getMessage();
+		}
+		
+		String[] tk;
+		
+		for (String elemento : dadosArquivo) {
+			tk = elemento.split(";");
+			Pesquisador p = new Pesquisador(tk[0], tk[1], tk[2], tk[3], 
+					tk[4], tk[5], tk[6]);
+			listaPesquisadores.add(p);
+		}
+	}
+	
+	/*
+	 * retorna lista de pesquisadores
+	 */
+	public Collection<Pesquisador> getListaPesquisadores(){
+		return listaPesquisadores;
+	}
+	
+	/*
+	 * retorna pesquisador por ID
+	 */
+	public Pesquisador getPesquisadorById(String id){
+		for (Pesquisador pesquisador : listaPesquisadores) {
+			if(pesquisador.getIdPesquisador().contentEquals(id)){
+				return pesquisador;
+			}
+		}
+		return null;
+	}
 }
--- src/main/java/pm/model/Pesquisador.java
+++ src/main/java/pm/model/Pesquisador.java
@@ -9,20 +9,20 @@ package pm.model;
  *
  * @author dhca
  */
-public abstract class Pesquisador {
+public class Pesquisador {
    
-    private int idPesquisador;
-    private char tag_titulacao;
-    private int horas_ic;
-    private int horas_estagio_docencia;
-    private int num_grad_orient;
-    private int num_M_orient;
-    private int num_D_orient;
+    private String idPesquisador;
+    private String tag_titulacao;
+    private String horas_ic;
+    private String horas_estagio_docencia;
+    private String num_grad_orient;
+    private String num_M_orient;
+    private String num_D_orient;
 
     
-    public Pesquisador(int idPesquisador, char tag_titulacao, int horas_ic,
-			int horas_estagio_docencia, int num_grad_orient, int num_M_orient,
-			int num_D_orient) {
+    public Pesquisador(String idPesquisador, String tag_titulacao, String horas_ic,
+    		String horas_estagio_docencia, String num_grad_orient, String num_M_orient,
+			String num_D_orient) {
 		this.idPesquisador = idPesquisador;
 		this.tag_titulacao = tag_titulacao;
 		this.horas_ic = horas_ic;
@@ -38,43 +38,38 @@ public abstract class Pesquisador {
      * 
      *********************/
     
-	public int getIdPesquisador() {
+	public String getIdPesquisador() {
 		return idPesquisador;
 	}
 
 
-	public char getTag_titulacao() {
+	public String getTag_titulacao() {
 		return tag_titulacao;
 	}
 
 
-	public int getHoras_ic() {
+	public String getHoras_ic() {
 		return horas_ic;
 	}
 
 
-	public int getHoras_estagio_docencia() {
+	public String getHoras_estagio_docencia() {
 		return horas_estagio_docencia;
 	}
 
 
-	public int getNum_grad_orient() {
+	public String getNum_grad_orient() {
 		return num_grad_orient;
 	}
 
 
-	public int getNum_M_orient() {
+	public String getNum_M_orient() {
 		return num_M_orient;
 	}
 
 
-	public int getNum_D_orient() {
+	public String getNum_D_orient() {
 		return num_D_orient;
 	}
-
-    
-    // Metodos a serem implementados
-    public abstract void popularidadePesquisador(Pesquisador p);
-    
     
 }
