4b25f76fa313c0565efa2f5f09a4e1c25bbf0889
Édipo Fernandes <edipofernandes7@gmail.com>
2015-04-02 21:10:54 -0300

    Classe Teste com a leitura de arquivo

---
+++ src/main/java/pm/main/ClasseTempParaTestes.java
@@ -0,0 +1,50 @@
+package pm.main;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ClasseTempParaTestes {
+	
+	private static String PATH = "testes-alunos/testes_15pesquisadores/entrada/";
+	
+	public static void main(String[] args) {
+		Collection<String> list = new ArrayList<String>();
+		
+		try{
+			FileReader arq = new FileReader(PATH + "pesquisadores.txt");
+			BufferedReader lerArq = new BufferedReader(arq);
+			
+			String linha = lerArq.readLine();//le a primeira linha do arquivo
+			list.add(linha);
+			
+			while(linha != null){
+				linha = lerArq.readLine();
+				list.add(linha);
+			}
+			
+			arq.close();
+			
+		}catch(IOException e){
+			System.out.println("Erro ao tentar abrir o arquivo: %s \n");
+			e.getMessage();
+		}
+		
+		System.out.println("Lendo a lista");
+		String[] c;
+		
+		for (String elemento : list) {
+			c = elemento.split(";");
+			if(c != null){
+				for (int i = 0; i < c.length; i++) {
+					String string = c[i];
+					System.out.print(string + " ");
+				}
+			}
+			
+			System.out.println("");
+		}
+	}
+}
